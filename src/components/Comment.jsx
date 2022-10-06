88c4f8a08a7235d6310ee00d277479011c431c7b
alfiansm07 <Rizal.yakul.2261@outlook.co.id>
2022-10-06 11:59:55 +0700

    0

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,62 +1,30 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { AiFillStar } from "react-icons/ai";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
 import { Pagination } from "swiper";
+import CommentItem from "./CommentItem";
 
 export default function Comments() {
   return (
     <>
       <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
         <SwiperSlide>
-          <div className="flex items-start justify-start w-full mb-10">
-            {/* kiri */}
-            <div className="font-black text-7xl text-main mr-10">"</div>
-            {/* kanan */}
-            <div>
-              {/* bintang */}
-              <div className="flex flex-row space-x-3 text-main">
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-              </div>
-              <p className="text-left mt-4 text-xs mr-10 leading-loose tracking-wider italic text-[#90A3B4] font-light">
-                “With Edu Smart, we can make it easier for you to master a
-                foreign language, it is important for us to grow and engage with
-                our students
-              </p>
-              <div className="flex items-center  ">
-                <img
-                  src="https://avatars.dicebear.com/api/male/john.svg?mood[]=happy&mood[]=sad"
-                  alt="avatar"
-                  className="w-10 h-10 rounded-full my-5"
-                />
-                <div className="flex flex-col text-left">
-                  <p className="text-main text-xs ">Jaquon Hart</p>
-                  <p className="text-[#90A3B4] text-xs font-extralight">
-                    Digital Marketing Executive, Hypebeast
-                  </p>
-                </div>
-              </div>
-            </div>
-            {/* end kanan */}
-          </div>
+          <CommentItem />
         </SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        <SwiperSlide>
+          <CommentItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CommentItem />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CommentItem />
+        </SwiperSlide>
       </Swiper>
     </>
   );
